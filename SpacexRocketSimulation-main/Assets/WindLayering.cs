8607473512239e5e9fb9ab0c4312d476d2285faf
ABHINAND A <abhinandhu0001@gmail.com>
2024-05-04 15:20:38 +0530

    abcd

--- SpacexRocketSimulation-main/Assets/WindLayering.cs
+++ SpacexRocketSimulation-main/Assets/WindLayering.cs
@@ -7,15 +7,14 @@ public class WindLayering : MonoBehaviour
 {
     [SerializeField]
     float windforce = 1000000f;
-    private void OnTriggerStay(Collider other)
-    {   
+    public Vector3 windDirection;
+    void OnTriggerStay(Collider other)
+    {
         var hitObj = other.gameObject;
         if (hitObj != null)
         {
             var rb = hitObj.GetComponent<Rigidbody>();
-            var dir = transform.up;
-            rb.AddForce(dir * windforce);
-            //Debug.Log("Wind force applied");
+            rb.AddForce(windDirection * windforce);
         }
     }
 
